a74ac22ca319510105da7d7242dd7519f05edc10
KAUSHALquantum002 <97720136+KAUSHALquantum002@users.noreply.github.com>
2023-11-26 19:26:48 +0530

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// Frontend/src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Assignment from './pages/Assignment';
+import Lecture from './pages/Lecture';
+import Notice from './pages/Notice';
+import Doc from './pages/Doc';
+import Complaint from './pages/Complaint';
+import Profile from './pages/Profile';
+import Users from './pages/Users';
+import Addstd from './pages/Addstd';
+import Addteach from './pages/Addteach';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      // <div style={{backgroundColor:"#000", height:"20vh", width:"20vw"}}></div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/assignments" element={<Assignment />} />
+          <Route path="/lectures" element={<Lecture />} />
+          <Route path="/notices" element={<Notice />} /> 
+          <Route path="/documents" element={<Doc />} /> 
+          <Route path="/complaints" element={<Complaint />} /> 
+          <Route path="/profiles" element={<Profile />} /> 
+          <Route path="/users" element={<Users />} /> 
+          <Route path="/addstudent" element={<Addstd />} /> 
+          <Route path="/addteacher" element={<Addteach />} /> 
+
+
+          
+        </Routes>
+      </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AuthProvider } from '../src/pages/authcontext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <AuthProvider>
+    <Router>
     <App />
+    </Router>
+    </AuthProvider>
   </React.StrictMode>
 );
 
